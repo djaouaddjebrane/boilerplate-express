770e3857ffb170a5ff1c6ed7325a8313b6b77809
xxdj21xx <88930584+djaouaddjebrane@users.noreply.github.com>
2023-12-11 14:38:17 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,5 +4,5 @@ let app = express();
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-
+app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
