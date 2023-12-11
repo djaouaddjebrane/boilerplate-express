00ba3a603e1396e9093678ee852cba31ce8ad024
xxdj21xx <88930584+djaouaddjebrane@users.noreply.github.com>
2023-12-11 15:44:34 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 app.use("/public", express.static(__dirname + "/public"));
+
+var message = "Hello json";
+
+app.get("/json", (req, res) => {
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
+    res.json({ message: message.toUpperCase() });
+  } else {
+    res.json({ message: message });
+  }
+});
+
 module.exports = app;
