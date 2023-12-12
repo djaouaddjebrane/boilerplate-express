7f51b16ea14cb7e13b9b9c8fee51a80c9539b7c1
xxdj21xx <88930584+djaouaddjebrane@users.noreply.github.com>
2023-12-12 10:40:43 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,20 +2,18 @@ require("dotenv").config();
 let express = require("express");
 let app = express();
 
+app.get("/name", function (req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  res.json({
+    name: `${firstName} ${lastName}`,
+  });
+});
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use("/public", express.static(__dirname + "/public"));
-
-var message = "Hello json";
-
-app.get("/json", (req, res) => {
-  if (process.env["MESSAGE_STYLE"] === "uppercase") {
-    res.json({ message: message.toUpperCase() });
-  } else {
-    res.json({ message: message });
-  }
-});
 
 module.exports = app;
